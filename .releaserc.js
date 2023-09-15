4c0fffdc0b7bf79774232a9e2e58af1fb58e9220
0xhashmap <114262299+0xhashmap@users.noreply.github>
2023-09-14 21:20:40 -0500

    fix: auto update package.json version after new release

--- .releaserc.js
+++ .releaserc.js
@@ -21,14 +21,14 @@ module.exports = {
 			},
 		],
 		"@semantic-release/release-notes-generator",
+		"@semantic-release/changelog",
+		"@semantic-release/npm",
 		[
 			"@semantic-release/git",
 			{
-				message:
-					"chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+				message: "chore(release): ${nextRelease.version} [skip ci]",
 			},
 		],
-		"@semantic-release/npm",
 		"@semantic-release/github",
 	],
 };
